a918ba56778af2c1b648d67881ea18f892666d69
Herrick-D <Herrick-D@users.noreply.github.com>
2017-09-25 10:39:24 -0400

    Initial Commit

---
+++ Study Abroad App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study Abroad App
+//
+//  Created by Dan Herrick on 9/25/17.
+//  Copyright © 2017 Dan Herrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
