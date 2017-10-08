379cd34e9a61d3e831978d0c60f7088a90a2ca4a
Herrick-D <Herrick-D@users.noreply.github.com>
2017-10-08 18:26:52 -0400

    Packing List With Parameters, still have run error to get to packing lists

--- Study Abroad App/ListDetailViewController.swift
+++ Study Abroad App/ListDetailViewController.swift
@@ -14,12 +14,33 @@ protocol ListDetailViewControllerDelegate: class {
     func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing packingList: PackingList)
 }
 
-class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+class ListDetailViewController: UITableViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var regionPickerText: UITextField!
+    @IBOutlet weak var lengthPickerText: UITextField!
+    @IBOutlet weak var seasonsPickerText: UITextField!
+    @IBOutlet weak var sexPickerText: UITextField!
+    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
+   
+    @IBOutlet weak var regionPicker: UIPickerView!
+    @IBOutlet weak var lengthPicker: UIPickerView!
+    @IBOutlet weak var seasonsPicker: UIPickerView!
+    @IBOutlet weak var sexPicker: UIPickerView!
     
     weak var delegate: ListDetailViewControllerDelegate?
     var packingListToEdit: PackingList?
+    
+    var regionPickerData = ["Northern Europe", "Greenland & Iceland", "Southern Europe", "Eastern Europe", "Western Europe, UK, & Ireland", "Russia", "West Asia", "East Asia", "South Asia", "Southeast Asia & Pacific Islands", "Australia", "New Zealand", "North Africa", "West Africa", "East Africa", "Central Africa", "South Africa", "North America", "Central America", "South America"]
+    
+    var lengthPickerData = ["1-3 weeks", "1-3 months", "4-6 months", "7-12 months", "over 1 year"]
+    
+    var seasonsPickerData = ["Autumn", "Winter", "Spring", "Summer", "Autumn + Winter", "Winter + Spring", "Spring + Summer", "Summer + Autumn", "Autumn + Winter + Spring", "Winter + Spring + Summer", "Spring + Summer + Autumn", "Summer + Autumn + Winter", "Autumn + Winter + Spring + Summer" ]
+    
+    var sexPickerData = ["Female", "Male", "Other"]
+    
+    let sections = ["Packing List Name", "Region", "Length of Trip", "Seasons", "Sex"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +70,16 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
         }
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    
+    
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         if indexPath.section == 1 {
             return indexPath
@@ -63,4 +94,77 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
         doneBarButton.isEnabled = (newText.length > 0)
         return true
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var countrows : Int = regionPickerData.count
+        if pickerView == lengthPicker {
+            countrows = self.lengthPickerData.count
+        }
+        else if pickerView == seasonsPicker {
+            countrows = self.seasonsPickerData.count
+        }
+        else if pickerView == sexPicker {
+            countrows = self.seasonsPickerData.count
+        }
+        return countrows
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == regionPicker {
+            let titleRow = regionPickerData[row]
+            return titleRow
+        }
+        else if pickerView == lengthPicker {
+            let titleRow = lengthPickerData[row]
+            return titleRow
+        }
+        else if pickerView == seasonsPicker {
+            let titleRow = seasonsPickerData[row]
+            return titleRow
+        }
+        else if pickerView == sexPicker {
+            let titleRow = sexPickerData[row]
+            return titleRow
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == regionPicker {
+            self.regionPickerText.text = self.regionPickerData[row]
+            self.regionPicker.isHidden = true
+        }
+        else if pickerView == lengthPicker {
+            self.lengthPickerText.text = self.lengthPickerData[row]
+            self.lengthPicker.isHidden = true
+        }
+        else if pickerView == seasonsPicker {
+            self.seasonsPickerText.text = self.seasonsPickerData[row]
+            self.seasonsPicker.isHidden = true
+        }
+        else if pickerView == sexPicker {
+            self.sexPickerText.text = self.sexPickerData[row]
+            self.sexPicker.isHidden = true
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if (textField == self.regionPickerText) {
+            self.regionPicker.isHidden = false
+        }
+        else if (textField == self.lengthPickerText) {
+            self.lengthPicker.isHidden = false
+        }
+        else if (textField == self.seasonsPickerText) {
+            self.seasonsPicker.isHidden = false
+        }
+        else if (textField == self.sexPickerText) {
+            self.sexPicker.isHidden = false
+        }
+    }
+    
 }
